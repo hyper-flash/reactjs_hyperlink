7b529529b81c02b9ab0d7b474e336f64e01aaecf
nodejs hyper backup <115861934+hyper-flash@users.noreply.github.com>
2022-11-14 21:44:49 +0530

    ...

--- scrapp/src/App.js
+++ scrapp/src/App.js
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Plyr from "plyr-react"
 import "plyr-react/plyr.css"
-import { CSSProperties } from "react";
+import "./App.css"
 import BeatLoader from "react-spinners/ClipLoader";
+import { Helmet } from "react-helmet";
 
 function App() {
 
@@ -21,20 +22,23 @@ function App() {
 
   function getUsers() {
     axios.get(
-      "https://rapid-stream.hyper-flash.workers.dev/https://streamtape.cc/v/jq2Zyqg71Ptzyml"
+      "https://rapid-stream.hyper-flash.workers.dev/https://streamtape.cc/v/3GyWopMPDrcdQx0 "
     )
       .then((response) => {
 
         var str = "" + response.data;
         var fullurl = str.match(/(?<=get_video\d*\s*).*?(?=\s*')/gm);
         var scrap_url = `https://streamtape.cc/get_video${fullurl[0]}&stream=1`;
-        if (scrap_url != 'https://streamtape.cc/get_video?id=j%22%20+&stream=1') {
+        if (fullurl[0].length > 30) {
           Geturl(scrap_url);
+          setInterval(() => {
+            setLoading(false)
+          }, 2000);
         } else {
-          Geturl("");
+          setLoading(true)
         }
-        console.log(scrap_url)
-        setLoading(false)
+        console.log(url)
+
       })
       .catch((error) => setLoading(false));
   }
@@ -45,13 +49,36 @@ function App() {
 
 
   if (isLoading) {
-    return  <><BeatLoader color="#36d7b7" /></>;
+    return <><div className="load"><BeatLoader color="#36d7b7" /></div></>;
+  } else {
+    return (
+      <>
+        <div id="myElement">
+          <Helmet><script>{`
+					    jwplayer("myElement").setup({
+								image: "https://content.jwplatform.com/thumbs/xJ7Wcodt-720.jpg",
+					    		aspectratio: "16:9",
+								width: '100%',
+								aspectratio: '16:9',
+					    		autostart: false,
+                  type: "mp4",
+				file : '${url}',
+				abouttext: 'FILMKACA',
+				aboutlink: 'http://filmkaca.xyz',
+				tracks:[{"file":"<?php echo $cc; ?>","label":"Indonesia","kind":"captions","default":"true"}],
+				captions: {color: '#ffb800',fontSize: 30,backgroundOpacity: 0},
+				sharing: {
+					sites: ['facebook','twitter','tumblr','googleplus',{icon: '//support-static.jwplayer.com/images/awesome.svg', src: 'http://www.jwplayer.com/?', label:'jwplayer'}],
+					code: encodeURI("<iframe src='<?php echo $sharing;?>' width='480' height='320'></iframe>"),
+					link: "<?php echo $sharing;?>"
+						}
+						    })`}
+          </script></Helmet>
+        </div>
+      </>
+    );
   }
-  return (
-    <>
-      <Plyr source={videoSrc} />
-    </>
-  );  
+
 }
 
 export default App;
