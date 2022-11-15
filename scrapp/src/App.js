111848a49d619e50d26a20b1e779e8757b27b791
nodejshyperbk <115861934+hyper-flash@users.noreply.github.com>
2022-11-15 12:02:04 +0530

    ...

--- scrapp/src/App.js
+++ scrapp/src/App.js
@@ -1,24 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Plyr from "plyr-react"
-import "plyr-react/plyr.css"
+import Plyr from 'plyr';
 import "./App.css"
 import BeatLoader from "react-spinners/ClipLoader";
-import { Helmet } from "react-helmet";
 
 function App() {
+  const player = new Plyr('#player');
 
   const [isLoading, setLoading] = useState(true);
   const [url, Geturl] = useState('');
-  const videoSrc = {
-    type: "video",
-    sources: [
-      {
-        src: url,
-        provider: "html5"
-      }
-    ]
-  };
 
   function getUsers() {
     axios.get(
@@ -29,18 +19,15 @@ function App() {
         var str = "" + response.data;
         var fullurl = str.match(/(?<=get_video\d*\s*).*?(?=\s*')/gm);
         var scrap_url = `https://streamtape.cc/get_video${fullurl[0]}&stream=1`;
-        if (fullurl[0].length > 30) {
-          Geturl(scrap_url);
-          setInterval(() => {
-            setLoading(false)
-          }, 2000);
-        } else {
-          setLoading(true)
-        }
-        console.log(url)
 
+        Geturl(scrap_url);
+        setInterval(() => {
+          setLoading(false)
+        }, 2000);
       })
-      .catch((error) => setLoading(false));
+      .catch((err) => {
+        setLoading(false)
+      });
   }
 
   useEffect(() => {
@@ -50,33 +37,35 @@ function App() {
 
   if (isLoading) {
     return <><div className="load"><BeatLoader color="#36d7b7" /></div></>;
-  } else {
-    return (
-      <>
-        <div id="myElement">
-          <Helmet><script>{`
-					    jwplayer("myElement").setup({
-								image: "https://content.jwplatform.com/thumbs/xJ7Wcodt-720.jpg",
-					    		aspectratio: "16:9",
-								width: '100%',
-								aspectratio: '16:9',
-					    		autostart: false,
-                  type: "mp4",
-				file : '${url}',
-				abouttext: 'FILMKACA',
-				aboutlink: 'http://filmkaca.xyz',
-				tracks:[{"file":"<?php echo $cc; ?>","label":"Indonesia","kind":"captions","default":"true"}],
-				captions: {color: '#ffb800',fontSize: 30,backgroundOpacity: 0},
-				sharing: {
-					sites: ['facebook','twitter','tumblr','googleplus',{icon: '//support-static.jwplayer.com/images/awesome.svg', src: 'http://www.jwplayer.com/?', label:'jwplayer'}],
-					code: encodeURI("<iframe src='<?php echo $sharing;?>' width='480' height='320'></iframe>"),
-					link: "<?php echo $sharing;?>"
-						}
-						    })`}
-          </script></Helmet>
-        </div>
-      </>
-    );
+  }
+  else {
+    console.log(url.length)
+    if (url.length < 70 || url.length == 0) {
+
+      return (
+        <>
+          <main className="flex min-h-screen w-full items-center justify-center bg-gray-100">
+            {/* component */}
+            <button className="group relative h-12 w-48 overflow-hidden rounded-lg bg-white text-lg shadow">
+              <div className="absolute inset-0 w-3 bg-red-500 transition-all duration-[250ms] ease-out group-hover:w-full" />
+              <button onClick={() => window.location.reload()}><span className="relative text-black group-hover:text-white">Please Refresh!</span></button>
+            </button>
+          </main>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <div>
+            <video id="player" width={"100%"} controls poster="https://m.media-amazon.com/images/I/81wmhpS+aRL._AC_SL1500_.jpg">
+              <source src={url} type="video/mp4" />
+            </video>
+          </div>
+
+        </>
+      );
+    }
+
   }
 
 }
